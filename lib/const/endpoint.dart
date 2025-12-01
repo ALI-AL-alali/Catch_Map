c2dd993623d85813beaec350e1e91f2dc051f48f
Ali_ALAli <150390545+09876540987654@users.noreply.github.com>
2025-12-01 11:02:37 +0300

    Update Api

---
+++ lib/const/endpoint.dart
@@ -0,0 +1,6 @@
+class EndPoint {
+  static const String baseUrl =
+      "https://tableghat.com/catch/DistanceCalculator/public";
+
+  static const String distance = "$baseUrl/distance-result";
+}
--- lib/services/distance_api_service.dart
+++ lib/services/distance_api_service.dart
@@ -1,28 +1,31 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:map/const/endpoint.dart';
 import '../models/distance_result_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DistanceApiService {
-  static const String baseUrl = "http://192.168.100.69:8000/distance-result";
-
   Future<DistanceResult?> getDistance({
     required LatLng from,
     required LatLng to,
   }) async {
     try {
-      final res = await http.post(
-        Uri.parse(baseUrl),
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode({
-          "from_lat": from.latitude,
-          "from_lng": from.longitude,
-          "to_lat": to.latitude,
-          "to_lng": to.longitude,
-        }),
-      );
+      final url = Uri.parse(EndPoint.distance);
 
-      print(res.body);
+      final request = http.Request("GET", url);
+      request.headers["Content-Type"] = "application/json";
+      request.body = jsonEncode({
+        "from_lat": from.latitude,
+        "from_lng": from.longitude,
+        "to_lat": to.latitude,
+        "to_lng": to.longitude,
+      });
+
+      final streamed = await request.send();
+      final res = await http.Response.fromStream(streamed);
+
+      print("STATUS: ${res.statusCode}");
+      print("BODY: ${res.body}");
 
       if (res.statusCode != 200) return null;
 
